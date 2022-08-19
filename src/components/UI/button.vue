8b1bb6f671e056d49c24408f77d1f65cb8cd929b
jonarroh <jonarrodi99@gmail.com>
2022-08-19 12:34:04 -0500

    🛠️ new feaute can read and write files

---
+++ src/components/UI/button.vue
@@ -0,0 +1,25 @@
+<template>
+	<button
+		:style="{ color: color, backgroundColor: bg }"
+		class="px-2 py-2 rounded-lg"
+	>
+		{{ Text }}
+	</button>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+	color: {
+		type: String,
+		default: '#fff'
+	},
+	bg: {
+		type: String,
+		default: '#171b25'
+	},
+	Text: {
+		type: String,
+		default: 'Button'
+	}
+});
+</script>
--- src/store/textStore.ts
+++ src/store/textStore.ts
@@ -2,15 +2,20 @@ import { defineStore } from 'pinia';
 
 interface TextStore {
 	inputText: string;
+	hasFile: boolean;
 }
 
 export const useTextStore = defineStore('textStore', {
 	state: (): TextStore => ({
-		inputText: '# Hello World!'
+		inputText: '# Hello World!',
+		hasFile: false
 	}),
 	actions: {
 		setInputText(text: string) {
 			this.inputText = text;
+		},
+		setHasFile(hasFile: boolean) {
+			this.hasFile = hasFile;
 		}
 	}
 });
